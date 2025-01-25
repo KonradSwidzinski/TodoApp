ff8cb914acd90dfdcdde3ddb9648f91f24a04b48
Konrad <konradswidzinski0@gmail.com>
2025-01-25 03:35:30 +0100

    first commit

---
+++ src/app/components/AddTask.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { AiOutlinePlus } from "react-icons/ai";
+import Modal from "./Modal";
+import { useState } from "react";
+
+const AddTask = () => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+    return (
+        <div>
+            <button onClick={() => setModalOpen(true)} className="btn btn-primary w-full">
+                Add new task<AiOutlinePlus className="ml-2" size={18} />
+            </button>
+            <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+                modal for adding todos
+            </Modal>
+        </div>
+    );
+};
+
+export default AddTask;
